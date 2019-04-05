aed9dae9b60919fc3733a757835af39d0aaa6a76
Kevin Flanagan <kflan3000@gmail.com>
2019-04-04 23:46:35 -0700

    obtain all restaurant variables from ajax call

--- assets/javascript/zomato.js
+++ assets/javascript/zomato.js
@@ -6,14 +6,16 @@ $(document).ready(function () {
     { "cuisine": { "cuisine_id": 1, "cuisine_name": "American" } },
     { "cuisine": { "cuisine_id": 73, "cuisine_name": "Mexican" } },
     { "cuisine": { "cuisine_id": 177, "cuisine_name": "Sushi" } },
-    { "cuisine": { "cuisine_id": 25, "cuisine_name": "Chinese" } },
-    { "cuisine": { "cuisine_id": 55, "cuisine_name": "Italian" } }
+    // { "cuisine": { "cuisine_id": 25, "cuisine_name": "Chinese" } },
+    // { "cuisine": { "cuisine_id": 55, "cuisine_name": "Italian" } }
+    {"cuisine":{"cuisine_id":95,"cuisine_name":"Thai"}},
+    {"cuisine":{"cuisine_id":82,"cuisine_name":"Pizza"}},
   ];
+
   var loading = false;
   const baseURL = 'https://developers.zomato.com/api/v2.1/';
   let q = '';
-  let cityArgs = 'cities?q=';
-  let cuisineArgs = ''
+  let cityArgs = 'cities?q=';  
   const ZapiKey = '100f8418181c52c8e7bd4b83d06f6750';
   let headers = {
     'user-key': ZapiKey,
@@ -22,7 +24,7 @@ $(document).ready(function () {
 
   // curl -X GET --header "Accept: application/json" --header "user-key: 100f8418181c52c8e7bd4b83d06f6750" "https://developers.zomato.com/api/v2.1/cities?q=phoenix"
   function searchCity(query) {
-    //TODO Add a loading feature to accomodate chained ajax calls.
+    //TODO Add a loading feature to accommodate chained ajax calls.
     loading = true;
     let queryURL = `${baseURL}${cityArgs}${query}`;
 
@@ -42,7 +44,7 @@ $(document).ready(function () {
           cityID = 301;
         }
         // console.log('cityID');
-        let queryURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city&q=&start=1&count=20&radius=1000&sort=rating&order=desc`;
+        let queryURL = `${baseURL}search?entity_id=${cityID}&entity_type=city&q=&start=1&count=6&radius=1000&sort=rating&order=desc`;
         // console.log(queryURL);
         //SECOND AJAX CAll is the chained in order to use the city ID to find foods if the type of cuisine
         return $.ajax({
@@ -61,8 +63,33 @@ $(document).ready(function () {
             }
             else {
               //TODO Populate the cards
-              $('#results-view').html(JSON.stringify(secondResponse));
               
+              //Was going to check length, but changed the API call to return only 6 results. May want to implement later.
+              // let cardsQuantity = 6;
+              // if (resultsFound < 6){
+              //   cardsQuantity = resultsFound;
+              // }
+                            
+              // Looping through restaurants               
+              for (const restaurant of secondResponse.restaurants) {
+                // variables for the query responses we want
+                let r = restaurant.restaurant;
+                let name = r.name;                
+                let latitude = r.location.latitude;
+                let longitude = r.location.longitude;
+                let imgsrc = r.thumb;
+                let cuisine = r.cuisine;
+                let userRating = r.user_rating;
+                let phoneNumber = r.phone_numbers;
+                
+                console.log(name);
+                //TODO create the cards
+
+
+              }
+              
+              // $('#results-view').html(restaurants);
+              // console.log(restaurants);
               //     <div class="uk-card uk-card-default">
               //     <div class="uk-card-media-top">
               //         <img src="images/light.jpg" alt="">
